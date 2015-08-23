af19a4d70a4a47c2fef9aecb8c11eb434c5261e7
Karen <min.karen@gmail.com>
2015-08-23 23:11:17 +0800

    initial commit to gh-pages

---
+++ submission.R
@@ -0,0 +1,9 @@
+pml_write_files = function(x){
+  n = length(x)
+  path <- "predictionAssignment_files/answers"
+  for(i in 1:n){
+    filename = paste0("problem_id_",i,".txt")
+    write.table(x[i],file=file.path(path, filename),quote=FALSE,row.names=FALSE,col.names=FALSE)
+  }
+}
+pml_write_files(results)
